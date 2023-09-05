ff2aa50322805afce0fd26b660ea660af09bc9d7
amtenu <amannov21@gmail.com>
2023-09-05 15:19:25 -0600

    route separated from app

--- app.js
+++ app.js
@@ -1,41 +1,47 @@
 const express = require("express");
-const path = require("path");
+const path=require('path');
 
-const mysql = require("mysql");
+const mysql=require("mysql");
 
-const dotenv = require("dotenv");
+const dotenv=require("dotenv")
 dotenv.config({
-  path: "./configs.env",
-});
+path:'./configs.env'
+})
 const app = express();
 
-const db = mysql.createConnection({
-  host: process.env.DATABASE_HOST,
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE,
+const db=mysql.createConnection({
+    host:process.env.DATABASE_HOST,
+    user:process.env.DATABASE_USER,
+    password:process.env.DATABASE_PASSWORD,
+    database:process.env.DATABASE
 });
-const publicDirectory = path.join(__dirname, "./public"); //current directory
+const publicDirectory = path.join(__dirname,'./public');//current directory
 
-app.use(express.static(publicDirectory));
-app.set("view engine", "hbs"); //view engine for html
+app.use(express.static(publicDirectory))
+app.set('view engine','hbs'); //view engine for html
 
-db.connect((error) => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log("Mysql connected....");
-  }
-});
 
-app.get("/", (req, res) => {
-  //res.send("<h1>Home Page</h1>");
-  res.render("index");
-});
-app.get("/register", (req, res) => {
-  //res.send("<h1>Home Page</h1>");
-  res.render("register");
-});
+
+db.connect((error)=>{
+if(error){
+    console.log(error)
+} else{
+    console.log("Mysql connected....")
+}
+})
+
+
+app.use('/',require('./routes/pages'));
+
+
+// app.get("/", (req, res) => {
+//   //res.send("<h1>Home Page</h1>");
+//   res.render('index');
+// });
+// app.get("/register", (req, res) => {
+//     //res.send("<h1>Home Page</h1>");
+//     res.render('register');
+//   });
 
 app.listen(5001, () => {
   console.log("Server started");
