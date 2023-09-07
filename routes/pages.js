bac7bf882766b8c45a0dd2c3a71a838b7e4bc09a
amtenu <amannov21@gmail.com>
2023-09-07 12:36:01 -0600

    Homepage while signed in , protected

--- routes/pages.js
+++ routes/pages.js
@@ -3,8 +3,10 @@ const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/auth");
 
-router.get("/", (req, res) => {
-  res.render("index");
+router.get("/", authController.isLoggedIn,(req, res) => {
+  res.render("index",{
+    user:req.user
+  });
 });
 
 router.get("/register", (req, res) => {
@@ -19,7 +21,7 @@ router.get("/login", (req, res) => {
 
 router.get("/profile", authController.isLoggedIn, (req, res) => {
   //console.log(req.message);  message is logged in and we can create many functions
- 
+ //database 
   if (req.user) {
     res.render("profile",{user:req.user});
   } else {
